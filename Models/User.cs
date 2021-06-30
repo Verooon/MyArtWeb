0084f96e76fca3ab137a970ec0fb90e5b44f4556
Veronika Tomasikova <veronika.tomasikova@vdp.sk>
2021-06-30 20:17:24 +0200

    Add files via upload

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyArtWeb.Models
+{
+    public class User
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Name { get; set; }
+        
+        [Key, MinLength(5)]
+        public string Email { get; set; } 
+        [Required, MinLength(8)]
+        public string Password { get; set; } //at least one capital letter, one letter, one number 
+        public string City { get; set; }
+
+    }
+}
---
+++ Models/UserList.cs
@@ -0,0 +1,61 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+ using MyArtWeb.Data;
+ using MyArtWeb2.Data;
+
+namespace MyArtWeb.Models
+{
+    public class UserList : IUserService
+    {
+        private List<User> users;
+        private User user;
+
+        public UserList()
+        {
+          
+        }
+
+        public void AddUser(User user)
+        {
+            if (ValidateRegistration())
+            {
+                users.Add(user);
+            }
+            throw new Exception("Invalid registration");
+        }
+
+
+        public User ValidateUser(string Email, string password)
+        {
+            User first = users.FirstOrDefault(user => user.Email.Equals(Email));
+            if (first == null)
+            {
+                throw new Exception("User not found");
+            }
+
+            if (!first.Password.Equals(password))
+            {
+                throw new Exception("Incorrect password");
+            }
+
+            return first;
+        }
+
+        public Boolean ValidateRegistration()
+        {
+            char ch1 = '@';
+            char ch2 = '.';
+            int index1 = user.Email.IndexOf(ch1);
+            int index2 = user.Email.IndexOf(ch2);
+            if (user.Email.Contains(ch1) && user.Email.Contains(ch2) && index1<index2) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
